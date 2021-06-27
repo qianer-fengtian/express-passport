94717fcd482b4af43adb4c42aa94a711bfaa6348
Fengtian <perplex0112358@icloud.com>
2021-06-27 16:51:13 +0900

    ログイン・ログアウトをモック実装

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  res.redirect('/');
+})
+
+router.post('/logout', function(req, res, next) {
+  res.redirect('/login');
+})
+
 module.exports = router;
